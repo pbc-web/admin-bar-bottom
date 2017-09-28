aeeb6680964d76d516432ed0653fefafc88a0bca
whytester <whytester@gmail.com>
2017-09-28 11:30:14 +0100

    Update pbc-bottom-admin-bar.php

--- pbc-bottom-admin-bar.php
+++ pbc-bottom-admin-bar.php
@@ -1,19 +1,19 @@
 <?php
 /*
-Plugin Name: PBC bottom admin bar
-Plugin URI: http://poweredbycoffee.co.uk/
+Plugin Name: Pbc bottom admin bar
+Plugin URI: http://poweredbycoffee.com/
 Description: Reposition the admin bar to the bottom of each page.
 Version: 1.0.0
 Author: Graeme White 
-Released under the GNU General Public License (GPL v2)
-https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+Released under the GNU General Public License (GPL)
+http://www.gnu.org/licenses/gpl.txt
 */
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 class pbcMoveAdminBar {
 
 	function __construct() {
-		add_action( 'wp_head', array( $this, 'move_admin_bar' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'move_admin_bar' ) );
 	}
 
 	function move_admin_bar() {
@@ -22,50 +22,8 @@ class pbcMoveAdminBar {
 			return;
 		}
 
-	    echo '
-	    <style type="text/css">
-	    body.admin-bar #wphead {
-	       padding-top: 0;
-	    }
-	    #wpadminbar {
-	        top: auto !important;
-	        bottom: 0;
-	        position: fixed;
-	    }
-	    #wpadminbar .quicklinks .menupop ul {
-	        position: absolute;
-	        bottom: 32px;
-	        background-color: #23282d;
-	    }
-	    #wpadminbar .quicklinks .menupop ul + ul {
-			bottom: 70px;
-		}
-	    #wpadminbar .quicklinks .menupop ul ul {
-			transform: translateY(62px);
-			-webkit-transform: translateY(62px);
-			-ms-transform: translateY(62px);
-		}
-		#wpadminbar .quicklinks .menupop ul.ab-sub-secondary {
-			bottom: 64px;
-			position: absolute;
-		}
-		@media screen and (max-width: 782px) {
-			#wpadminbar .quicklinks .menupop ul {
-				bottom: 46px;
-			}
+	    wp_enqueue_style( 'pbc-move-admin-bar' , plugin_dir_url( __FILE__ ) . 'css/pbc-bottom-admin-bar.css' , array() , 1.00 , false );
 
-			#wpadminbar .quicklinks .menupop ul + ul,
-			#wpadminbar .quicklinks .menupop ul.ab-sub-secondary {
-				bottom: 86px;
-			}
-
-			#wpadminbar .quicklinks .menupop ul ul {
-				transform: translateY(92px);
-				-webkit-transform: translateY(92px);
-				-ms-transform: translateY(92px);
-			}
-		}
-	    </style>';
 	}
 }
 
